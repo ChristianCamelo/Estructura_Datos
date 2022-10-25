2d64bcff0e4d31ad10978e23d9c93fe8e5a85ede
CrnlMcPatatas <116672498+CrnlMcPatatas@users.noreply.github.com>
2022-10-25 20:19:18 +0200

    Update Lista.h

--- Lista.h
+++ Lista.h
@@ -2,6 +2,9 @@
 #define Lista_H
 
 #include "PLoc.h"
+
+using namespace std;
+
 class Lista{
 
     private:
@@ -11,17 +14,50 @@ class Lista{
                 PLoc pd;
                 Nodo *next;
                 Nodo *prev;
-                Nodo();
-                Nodo(PLoc p);
-                PLoc getLocalidad();
-                friend ostream &operator<<(ostream &os, Nodo &n);
-        }
+                Nodo(){
+                    next = NULL;
+                    prev = NULL;
+                    
+                }
+                Nodo(PLoc p){
+                        pd=p;
+                        next=NULL;
+                        prev=NULL;
+                }
+                PLoc getLocalidad(){
+                    return pd;
+                }
+                friend ostream &operator<<(ostream &os, Nodo &n){
+                    cout<<n.pd<<endl;
+                    return os;
+                }
+        };
 
-        Nodo *pr
+        Nodo *pr;
         Nodo *ul;
 
     public:
-
-
+        //      **canonicos**
+        Lista();
+        //Lista(Nodo &n); <-- no lo requiere
+        Lista(const Lista &l);
+        ~Lista();
+        Lista & operator=(const Lista &l);
+        //      **metodos de instancia**
+        bool esVacia();
+        void leeLista(string f);
+        void insertaCabeza(PLoc p);
+        void insertaCOla(PLoc p);
+        void insertaVectorPLoc(vector<PLoc>&v);
+        void inserta(PLoc v,int i);
+        bool borraCabeza();
+        bool borraCola();
+        bool borraCiudad(string v);
+        bool borraPais(string s);
+        int ciudadEnLista(string v);
+        vector<PLoc> pais(string p);
+        void ordenaLista();
+        friend ostream &operator <<(ostream &, const Lista &);
+        void escribeListaPais(string p);
 };
 #endif
