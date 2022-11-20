4c80508695ec8d5e1a4315ac2b94473071010c33
CrnlMcPatatas <116672498+CrnlMcPatatas@users.noreply.github.com>
2022-11-20 15:53:24 +0100

    Add files via upload

--- Lista.h
+++ Lista.h
@@ -1,12 +1,13 @@
 #ifndef Lista_H
 #define Lista_H
-
+//                                      Por Christian Camelo Espinal
 #include "PLoc.h"
 
 using namespace std;
 
 class Lista{
-
+    
+    friend ostream &operator <<(ostream &, const Lista &);
     private:
 
         class Nodo{
@@ -14,6 +15,9 @@ class Lista{
                 PLoc pd;
                 Nodo *next;
                 Nodo *prev;
+                ~Nodo(){
+                    delete this;
+                }
                 Nodo(){
                     next = NULL;
                     prev = NULL;
@@ -28,7 +32,8 @@ class Lista{
                     return pd;
                 }
                 friend ostream &operator<<(ostream &os, Nodo &n){
-                    cout<<n.pd<<endl;
+                    PLoc p = n.getLocalidad();
+                    os<<p.getContinente()<<" "<<p.getPais()<<" "<<p.getCiudad()<<" "<< p.getGps()[0] << " " << p.getGps()[1] << endl;
                     return os;
                 }
         };
@@ -40,24 +45,25 @@ class Lista{
         //      **canonicos**
         Lista();
         //Lista(Nodo &n); <-- no lo requiere
-        Lista(const Lista &l);
+        Lista(Lista const &l);
         ~Lista();
         Lista & operator=(const Lista &l);
         //      **metodos de instancia**
         bool esVacia();
         void leeLista(string f);
         void insertaCabeza(PLoc p);
-        void insertaCOla(PLoc p);
+        void insertaCola(PLoc p);
         void insertaVectorPLoc(vector<PLoc>&v);
         void inserta(PLoc v,int i);
         bool borraCabeza();
         bool borraCola();
         bool borraCiudad(string v);
         bool borraPais(string s);
+        PLoc operator[](int pos);
         int ciudadEnLista(string v);
         vector<PLoc> pais(string p);
         void ordenaLista();
-        friend ostream &operator <<(ostream &, const Lista &);
         void escribeListaPais(string p);
+        void buscaCercana(double ,double);
 };
 #endif
